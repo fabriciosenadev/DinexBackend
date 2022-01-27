f7c1f100dc170040f77923bd12694b5768a19f85
Fabricio Sena <fabricior.sena@outlook.com>
2022-01-27 05:43:08 -0300

    add obrigatoriedade aos campos de usuario

--- Dinex.WebApi/API/Models/User/UserInputModel.cs
+++ Dinex.WebApi/API/Models/User/UserInputModel.cs
@@ -2,9 +2,16 @@
 {
     public class UserInputModel
     {
+        [Required]
         public string FullName { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string VerifyPassword { get; set; }
     }
 }
