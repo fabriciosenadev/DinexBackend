946ab2f8e6a4af3b86d04c8b4efb1d62372f616e
Fabricio Sena <fabricior.sena@outlook.com>
2022-02-25 23:08:27 -0300

    add settings to get template from activation account

--- Dinex.WebApi/Infra/Settings/AppSettings.cs
+++ Dinex.WebApi/Infra/Settings/AppSettings.cs
@@ -2,6 +2,16 @@
 {
     public class AppSettings
     {
+        // -- JWT Secret
         public string Secret { get; set; }
+
+        // -- SendMail settings
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+        public bool SmtpUseSsl { get; set; }
+        public string MailboxAddress { get; set; }
+        public string MailboxPassword { get; set; }
+        public string MailboxName { get; set; }
+        public string MailTemplateFolder { get; set; }
     }
 }
