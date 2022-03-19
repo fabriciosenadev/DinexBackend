2fb90f48be6e48a26226a3001313d54f6fd09635
Fabricio Sena <fabricior.sena@outlook.com>
2022-03-18 23:13:04 -0300

    add entity to relation user and category

---
+++ Dinex.WebApi/Entities/Category/CategoryToUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Dinex.WebApi.Entities
+{
+    public class CategoryToUser
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
