ad8cb3900b837e68ba7e9f8ac16ba7ef3e845d19
Fabricio Sena <fabricio.sena@dcnexxy.net.br>
2022-06-28 18:53:45 -0300

    fix: filter by deletedAt with user id too

--- Dinex.Infra/Repositories/LaunchRepository.cs
+++ Dinex.Infra/Repositories/LaunchRepository.cs
@@ -18,7 +18,7 @@
         public async Task<List<Launch>> ListLast(Guid userId)
         {
             var result = await _context.Launches
-                .Where(x => x.UserId.Equals(userId))
+                .Where(x => x.UserId.Equals(userId) && x.DeletedAt == null)
                 .OrderByDescending(x => x.Date)
                 .OrderByDescending(x => x.CreatedAt)
                 .Take(8)
