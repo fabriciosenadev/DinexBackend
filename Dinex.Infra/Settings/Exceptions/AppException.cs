0e2e3c8d59f4f0b924e98db4dabed47288530ecd
Fabricio Sena <fabricior.sena@outlook.com>
2022-06-29 08:33:36 -0300

    feature: implement new structure of exceptions

--- Dinex.Infra/Settings/Exceptions/AppException.cs
+++ Dinex.Infra/Settings/Exceptions/AppException.cs
@@ -6,14 +6,14 @@
         {
 
         }
-        
+
         public AppException(string message) : base(message)
         {
 
         }
-        
-        public AppException(string message, params object[] args) 
-            : base(String.Format(CultureInfo.CurrentCulture, message, args))
+
+        public AppException(string message, params object[] args)
+            : base(string.Format(CultureInfo.CurrentCulture, message, args))
         {
 
         }
