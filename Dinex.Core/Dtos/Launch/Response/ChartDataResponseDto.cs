746088e21d1cadbf6e9f5ad2fc7a6ede958b08e8
Fabricio Sena <fabricior.sena@outlook.com>
2022-07-17 17:53:33 -0300

    change Dto structure

---
+++ Dinex.Core/Dtos/Launch/Response/ChartDataResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Dinex.Core
+{
+    public class ChartDataResponseDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public decimal Amount { get; set; }
+        public string Applicable { get; set; }
+        public string PayMethod { get; set; }
+    }
+}
---
+++ Dinex.Core/Dtos/Launch/Response/LaunchDetailsByYearAndMonthResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Dinex.Core
+{
+    public class LaunchDetailsByYearAndMonthResponseDto
+    {
+        public List<LaunchResponseDto> Launches { get; set; }
+        public List<ChartDataResponseDto> PieChartData { get; set; }
+    }
+}
