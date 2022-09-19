59d5fbe416bea6387d5768ffbf03915b3b2519e8
Fabricio Sena <fabricio.sena@dcnexxy.net.br>
2022-09-19 19:46:37 -0300

    implement infra unit tests

---
+++ test/Dinex.Infra.Test/Mocks/UserMock.cs
@@ -0,0 +1,24 @@
+﻿namespace Dinex.InfraTests
+{
+    [ExcludeFromCodeCoverage]
+    public class UserMock
+    {
+        private readonly Faker _faker;
+
+        internal UserMock(Faker faker)
+        {
+            _faker = faker;
+        }
+
+        internal User GetUserMock()
+        {
+            return new User { 
+                FullName = _faker.Name.FullName(),
+                Email = _faker.Internet.Email(),
+                IsActive = UserActivatioStatus.Inactive,
+                Password = _faker.Lorem.Word(),
+            };
+
+        }
+    }
+}
---
+++ test/Dinex.Infra.Test/Usings.cs
@@ -0,0 +1,10 @@
+global using System.Diagnostics.CodeAnalysis;
+
+global using Xunit;
+
+global using Bogus;
+
+global using NSubstitute;
+
+global using Dinex.Core;
+global using Dinex.Infra;
