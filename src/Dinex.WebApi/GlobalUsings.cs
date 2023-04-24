c7f598951c78fbb48fe1d85dec48341d79b6d883
Fabricio Sena <fsena@kornerstonecredit.com>
2023-04-23 22:46:08 -0300

    remove using non used

--- src/Dinex.WebApi/GlobalUsings.cs
+++ src/Dinex.WebApi/GlobalUsings.cs
@@ -4,8 +4,6 @@ global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Options;
 
-global using AutoMapper;
-
 global using Dinex.Business;
 global using Dinex.Core;
 global using Dinex.Infra;
